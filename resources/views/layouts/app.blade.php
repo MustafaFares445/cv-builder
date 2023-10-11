de918236b3e6de5a74ae7a07d7e36da362e0006f
Mustafa Fares <mustaphafares445@gmail.com>
2023-10-11 15:41:54 +0300

    Adding wire-elements/spotlight

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,13 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
+        @livewireStyles
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @livewireStyles
+
+        @livewireScripts
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -31,7 +33,8 @@
             <main>
                 {{ $slot }}
             </main>
-            @livewireScripts
+
         </div>
+        @livewire('livewire-ui-spotlight')
     </body>
 </html>
